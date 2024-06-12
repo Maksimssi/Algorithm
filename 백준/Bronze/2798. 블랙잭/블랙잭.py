3d2b75588b61569224de369f50eb2288981ce7d4
막심 <142511332+Maksimssi@users.noreply.github.com>
2024-06-12 18:32:02 +0900

    [Bronze II] Title: 블랙잭, Time: 92 ms, Memory: 39860 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,16 +1,11 @@
+from itertools import combinations
 import sys
 
 cards, goal = map(int, input().split())
 
-card_list = list(map(int,sys.stdin.readline().split()))
-result = 0
+list = sorted(list(map(sum,combinations(map(int, sys.stdin.readline().split()), 3))),reverse=True)
 
-for i in range(cards):
-    for j in range(i+1,cards):
-        for k in range(j+1,cards):
-            if not result:
-                result = card_list[i]+card_list[j]+card_list[k]
-            elif goal >= card_list[i]+card_list[j]+card_list[k]:
-                if abs(goal - result) > abs(goal - (card_list[i]+card_list[j]+card_list[k])):
-                    result = card_list[i]+card_list[j]+card_list[k]
-print(result)
+for i in list:
+    if i <= goal:
+        print(i)
+        break
