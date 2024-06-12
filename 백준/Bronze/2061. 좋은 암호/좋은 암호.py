f231c24eb485e7ab152133d904e68d1d1857ce71
막심 <142511332+Maksimssi@users.noreply.github.com>
2024-06-12 20:13:00 +0900

    [Bronze III] Title: 좋은 암호, Time: 268 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2061. 좋은 암호/좋은 암호.py
+++ 백준/Bronze/2061. 좋은 암호/좋은 암호.py
@@ -1,12 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-k, l = input().split()
-L = int(l)
+k, l = map(int,input().split())
 is_bad = False
-# 2부터 L까지 나누어 떨어진다면 (BAD + 인수) 출력 
-for i in range(2, L):
-    if int(k) % i == 0:
+# 2부터 L까지 나누어 떨어진다면 (BAD + 인수) 출력
+for i in range(2, l):
+    if k % i == 0:
         print("BAD", i)
         is_bad = True
         break
