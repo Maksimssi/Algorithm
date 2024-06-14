9fec43887ce415083d0d3383d4a3702009d94391
막심 <142511332+Maksimssi@users.noreply.github.com>
2024-06-14 11:56:29 +0900

    [Bronze III] Title: 지각, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10419. 지각/지각.py
+++ 백준/Bronze/10419. 지각/지각.py
@@ -2,7 +2,7 @@ T = int(input())
 
 for i in range(T):
     d = int(input())  # 수업시간
-    for j in range(d+1):
+    for j in range(10001):
         if j + j**2 > d:
             print(j-1)
             break
