98752203c6576dd3e3a21381be80973508bf3b87
막심 <142511332+Maksimssi@users.noreply.github.com>
2024-06-14 15:52:31 +0900

    [Bronze II] Title: 거스름돈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -5,8 +5,10 @@ my_changes = account - price
 changes_list = [500, 100, 50, 10, 5, 1]
 money_count = 0
 
-for changes in changes_list:
-    money_count += my_changes // changes
-    my_changes %= changes
+def calculate(money_count,my_changes):
+    for changes in changes_list:
+        money_count += my_changes // changes
+        my_changes %= changes
+    return money_count
 
-print(money_count)
+print(calculate(money_count,my_changes))
